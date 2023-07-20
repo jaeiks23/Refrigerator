99adc1510b3a68923102f8c9d31d0e06cecf99c7
jaeiks23 <131107367+jaeiks23@users.noreply.github.com>
2023-07-20 13:18:37 +0900

    Delete ref 0717 directory

--- ref 0717/refrigerator/build/resources/main/static/js/find.js
+++
@@ -1,4 +0,0 @@
-let container = document.getElementById("container");
-window.addEventListener('load', () => {
-    container.classList.add('upscale');
-})
--- ref 0717/refrigerator/out/production/resources/static/js/mypage.js
+++
@@ -1,9 +0,0 @@
-function toggle_menu(){
-    const submenu = document.getElementById("myMenu");
-    if(submenu.style.display == 'none'){
-        submenu.style.display = 'block';
-    }
-    else{
-        submenu.style.display = 'none';
-    }
-};
